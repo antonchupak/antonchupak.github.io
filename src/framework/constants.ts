f6e78fd2ffe0fd2b1aff19c471b082c0bb8c9e25
Tony Chupak <tony.chupak@gmail.com>
2019-12-18 14:27:31 +0200

    Updates

--- src/framework/constants.ts
+++
@@ -1,5 +0,0 @@
-export const HELLO_TEXT = {
-  name: "Anton Chupak",
-  position: "Front-end developer",
-  description: "I'm a developer based in Kiev, Ukraine. Specialized in developing web applications in any complexity. Currently focused on React."
-};
